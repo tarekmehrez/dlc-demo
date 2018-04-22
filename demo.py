2b642911b0436bfa48cf7fe35503794f4125aa63
Tarek Mehrez <mehrez@textkernel.nl>
2018-04-22 11:56:33 +0200

    added port as a param

--- demo.py
+++ demo.py
@@ -1,6 +1,16 @@
+import argparse
+import sys
+
 from flask import Flask
 
+
 app = Flask(__name__)
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--port',
+                    dest='port',
+                    type=int,
+                    help='port to listen on')
 
 
 @app.route('/v1/hello')
@@ -9,4 +19,8 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    args = parser.parse_args()
+    if not args.port:
+        parser.print_help()
+        sys.exit(0)
+    app.run(host='0.0.0.0', port=args.port)
